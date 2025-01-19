b68d02482ce49f10923dfd79652d9e53f197c39f
HasnainAliMuhammadAshrafChandio <ahchandio23@gmail.com>
2025-01-19 11:50:37 -0800

    addfiles

--- my-next-app/next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_CLERK_FRONTEND_API: "<your-frontend-api>", // Replace with your API key
-  },
-};
-
-export default nextConfig;
